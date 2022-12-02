1509ec346bc4f392300b0af177d7d41be28a77ec
rhKashif <118441551+rhKashif@users.noreply.github.com>
2022-12-02 12:56:52 +0000

    Bug Fix to reset number of Days when resetting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
 let count = 0;
-let Day = 1;
+let day = 1;
 
 function increment() {
     count += 1;
@@ -17,7 +17,8 @@ function save() {
 }
 
 const reset = () =>{
-    count =0;
+    count = 0;
+    day = 0;
     saveEl.textContent = "Snooze History:";
     countEl.textContent = 0;
 }
